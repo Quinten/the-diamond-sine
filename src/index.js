fb6f5b071f918390896368b3ae83ca1070a0ba0d
Quinten Clause <quintenclause@hotmail.com>
2021-06-26 19:15:40 +0200

    A bit of golfing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 u = 24;
-uu = u * u;
+U = u * u;
 v = u / 2;
-vv = v * v;
-q = Math.sqrt(vv * 2);
+q = Math.sqrt(v * v * 2);
 
 // colors
 b.style.background = '#334';
 c.fillStyle = '#eff';
 
 // centering
-c.translate((a.width - uu) / 2, (a.height - uu) / 2);
+c.translate((a.width - U) / 2, (a.height - U) / 2);
 
 P = Math.PI;
-V = c.fillRect.bind(c);
+T = c.fillRect.bind(c);
 R = c.rotate.bind(c);
 
 // sprites
@@ -22,16 +21,16 @@ g = [
     _ => _,
 
     // 1 dirt
-    _ => V(-v, -v, u - 1, u - 1),
+    _ => T(-v, -v, u - 1, u - 1),
 
     // 2 boulder
     _ => [c.beginPath(), c.arc(0, 0, v - 1, 0, P*2), c.fill()],
 
     // 3 daimond
-    _ => [R(P/4), V(-q/2, -q/2, q, q)],
+    _ => [R(P/4), T(-q/2, -q/2, q, q)],
 
     // 4 player
-    _ => [R(P/4), V(-q/2, -q/6, q, q/3), R(P/2), V(-q/2, -q/6, q, q/3)]
+    _ => [R(P/4), T(-q/2, -q/6, q, q/3), R(P/2), T(-q/2, -q/6, q, q/3)]
 ];
 
 // pseudo random number generator
@@ -45,10 +44,10 @@ z = _ => {
     // reset prng
     r = m($);
     // start position
-    s = r() * uu | 0;
+    s = r() * U | 0;
     // grid
     p = [];
-    while (p.length < uu) {
+    while (p.length < U) {
         p.push(
             // player
             (p.length == s) ? 4 : // player
@@ -77,9 +76,9 @@ E = (g, G, l) => {
 
 // main loop
 f = _ => {
-    c.clearRect(-1, -1, uu + 2, uu + 2);
+    c.clearRect(-1, -1, U + 2, U + 2);
     y = 0;
-    while (y < uu) {
+    while (y < U) {
         x = 0;
         while (x < u) {
             // drop boulders
@@ -123,7 +122,7 @@ onkeyup = e => {
     n = s + w;
     // wrapping player around the playing field
     k = ((s / u | 0) - (n / u | 0)) * u * (w * w % u);
-    n = (n + uu + k) % uu;
+    n = (n + U + k) % U;
 
     // step on certain tiles
     if (!!(p[n] % 2) || p[n] == 0) {
